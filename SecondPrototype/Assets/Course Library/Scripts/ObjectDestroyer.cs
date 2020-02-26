3d940ab24bf5365c3041071928cd04c308bd7882
remiznik <remzinik@gmail.com>
2020-02-26 20:38:55 +0300

    add SecondPrototype

---
+++ SecondPrototype/Assets/Course Library/Scripts/ObjectDestroyer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroyer : MonoBehaviour
+{
+    public float bottomZLimit;
+    public float topZLimit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topZLimit)
+        {
+            Destroy(gameObject);
+        }
+
+        if (transform.position.z < bottomZLimit)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ SecondPrototype/Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject playerProjectile;
+    public float horizontalInput;
+    public float speed = 10f;
+    public float playerXLimit = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+        if (transform.position.x <  -playerXLimit)
+        {
+            transform.position = new Vector3(-playerXLimit, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > playerXLimit)
+        {
+            transform.position = new Vector3(playerXLimit, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(playerProjectile, transform.position, transform.rotation);
+        }
+    }
+}
