e903b73154f5cde3f24c3a1c130b44b3010131ea
remiznik <remzinik@gmail.com>
2020-03-04 20:17:32 +0300

    assets

---
+++ SecondPrototype/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float delta = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        delta += Time.deltaTime;
+        
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (delta > 1)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                delta = 0;
+            }              
+        }
+        
+    }
+}
